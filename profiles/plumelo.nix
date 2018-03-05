3389f26fe87238a3cb6e083d4f74f15b56af29be
Unknown <iulian.meghea@gmail.com>
2018-03-05 09:17:02 +0200

    Enable docker
    Fix docker conf
    
    
    Default group in example

--- profiles/plumelo.nix
+++ profiles/plumelo.nix
@@ -7,6 +7,7 @@
     # config
     ../modules/virtualisation/lxc.nix
     ../modules/virtualisation/lxd.nix
+    ../modules/virtualisation/docker.nix
     ../modules/hardware/ssd.nix
     ../modules/hardware/zram.nix
   ];
