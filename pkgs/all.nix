6de37c1f26b8686fa31039dba310094982f5a7ba
Unknown <iulian.meghea@gmail.com>
2018-03-27 23:47:41 +0300

    Add it87 linux module
    it

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -3,6 +3,7 @@
   imports = [
     ./unstable.nix
     ./linux/linux-4.16.nix
+    ./linux/it87.nix
     ./lm-sensors/lm-sensors.nix
   ];
 }
---
+++ pkgs/linux/it87.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }: 
+ 
+let 
+  unstable = import <nixos-unstable> { 
+    config.allowUnfree = true; 
+  };
+  kernel = config.boot.kernelPackages.kernel; 
+in { 
+  nixpkgs.overlays = [( self: super: { 
+    it87  = unstable.stdenv.mkDerivation  rec { 
+      name = "it87-${version}"; 
+      version = "1.0"; 
+      src = unstable.fetchFromGitHub { 
+        owner = "groeck"; 
+        repo = "it87"; 
+        rev = "master"; 
+        sha256 = "1kxhwpfv9324828bngviacyl05jlrkqryppr1h257nhypy9rpxyg"; 
+      }; 
+      hardeningDisable = ["pic"]; 
+     nativeBuildInputs = self.linux_4_16_rc.kernel.moduleBuildDependencies; 
+ 
+      preBuild = '' 
+          substituteInPlace Makefile --replace "\$(shell uname -r)" "${kernel.modDirVersion}"
+          substituteInPlace Makefile --replace "/lib/modules" "${kernel.dev}/lib/modules"
+          substituteInPlace Makefile  --replace depmod \#  
+          substituteInPlace Makefile --replace '$(MODDESTDIR)' "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/hwmon/" 
+        ''; 
+      preInstall = '' 
+        mkdir -p "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/hwmon" 
+      ''; 
+    };
+  })];
+}
