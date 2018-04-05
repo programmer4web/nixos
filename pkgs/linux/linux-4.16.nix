17985630c26db46dab637a0ffb0946d76e530405
Unknown <iulian.meghea@gmail.com>
2018-04-05 10:06:03 +0300

    Linux kernel 4.16

--- pkgs/linux/linux-4.16.nix
+++ pkgs/linux/linux-4.16.nix
@@ -7,18 +7,18 @@ let
 in
 {
   nixpkgs.overlays = [( self: super: {
-    linux_4_16_rc = unstable.linuxPackagesFor (unstable.callPackage <nixos-unstable/pkgs/os-specific/linux/kernel/linux-testing.nix> {
+    linux_4_16 = unstable.linuxPackagesFor (unstable.callPackage <nixos-unstable/pkgs/os-specific/linux/kernel/linux-testing.nix> {
       kernelPatches = with unstable; [
         kernelPatches.bridge_stp_helper
         kernelPatches.modinst_arg_list_too_long
       ];
       argsOverride = with unstable; rec {
-        version = "4.16-rc7";
-        modDirVersion = "4.16.0-rc7";
+        version = "4.16";
+        modDirVersion = "4.16.0";
         extraMeta.branch = "4.16";
         src = fetchurl {
           url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-          sha256 = "13zpfjxd38202afjl6flc9brjw3sp4sfq3wls0v90k1i2b308qfi";
+          sha256 = "0crdsyx42s5fw2gqzkh5ahj67pjw3fks5zannvixl65ycyziq74k";
         };
       };
     });
