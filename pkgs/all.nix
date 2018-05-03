1508ed99989898f1bb43afa9678492e9e5bfcfc7
Unknown <iulian.meghea@gmail.com>
2018-05-03 13:58:09 +0300

    Remove unstable usage

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
-    ./unstable.nix
     ./linux/linux-4.17.nix
     ./linux/it87.nix
     ./lm-sensors/lm-sensors.nix
--- profiles/profile.nix
+++ profiles/profile.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, ... }:
-
-let
-  unstable = import <nixos-unstable> {
-    config.allowUnfree = true;
-  };
-
-in {
+{
   imports = [
     ./local.nix
     ./plumelo.nix
